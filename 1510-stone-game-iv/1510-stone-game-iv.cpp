b491e91035f4b016ccd3d1a4e59e6f442180752b
Sanju <78341083+Sanju-github002@users.noreply.github.com>
2022-01-22 21:34:04 +0530

    Time: 268 ms (23.82%), Space: 15.9 MB (11.75%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int dp[1000001];
+    int helper(int n){
+      if(n <= 0) return 0;
+       if(dp[n] != -1) 
+          return dp[n];
+        for(int i=1;i*i<=n;i++){
+            if(helper(n-i*i)==0) return dp[n] = 1;
+        }
+        return 0;
+    }
+    bool winnerSquareGame(int n) {
+        memset(dp, -1,sizeof dp);
+        return helper(n);
+    }
+};
