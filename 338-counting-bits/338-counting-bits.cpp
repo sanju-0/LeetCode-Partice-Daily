64b663e6f138291867d7dd48e1812f06e3009258
Sanju <78341083+Sanju-github002@users.noreply.github.com>
2022-03-01 21:59:37 +0530

    Time: 4 ms (88.16%), Space: 7.9 MB (48.88%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+         //mem[i] = No of 1s from 0 to number i
+        vector<int> ans(n+1);
+        ans[0] = 0;
+        
+        for(int i=1;i<=n;++i)
+            ans[i] = ans[i/2] + i%2;
+        
+        return ans;
+    }
+};
