11edc364821af51df0310f5486fa9296f05431f0
Sanju <78341083+Sanju-github002@users.noreply.github.com>
2022-04-14 13:17:32 +0530

    Time: 7 ms (73.23%), Space: 8.7 MB (68.08%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+      for(int j=triangle.size()-2; j>=0; j--){
+          for(int i=0;i<=j;i++){
+              triangle[j][i] += min(triangle[j + 1][i], triangle[j + 1][i + 1]);
+          }
+      }
+        return triangle[0][0];
+    }
+};
+
+// We start from the down to uper and at the one node we get the min distance.
