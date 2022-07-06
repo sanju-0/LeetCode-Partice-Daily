b8b229a56c9a2de025d6c5fee5416c7eecf045ce
Sanju <anutanwer12@gmail.com>
2022-07-06 12:16:32 +0530

    Time: 493 ms (29.65%), Space: 10.3 MB (97.83%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-    int n = nums.size(), ans = 1;
-     vector<int> dp(n,1);
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j] && dp[i]<dp[j]+1)
-                    dp[i] =1+dp[j];
-                ans = max(ans, dp[i]);
-            }
-        }
-        return ans;
+      int n = nums.size();
+      int lis[n];
+      lis[0] = 1;
+      for(int i=1;i<n;i++){
+         lis[i] = 1;
+         for(int j=0;j<i;j++){
+           if(nums[i] > nums[j] and lis[i] < lis[j]+1) 
+              lis[i] = lis[j] + 1;
+         }
+      }
+        return *max_element(lis,lis+n);
     }
 };
-
-/*
-   int n = nums.size(), ans = 1;
-     vector<int> dp(n,1);
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j] && dp[i]+1 > dp[j])
-                    dp[i] = max(dp[i],dp[j]+1);
-                ans = max(ans, dp[i]);
-            }
-        }
-        return ans;
-*/
