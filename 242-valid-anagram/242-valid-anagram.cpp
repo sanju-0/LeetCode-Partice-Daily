a5d0da5b797ea609da2b21286edea0649ad58645
Sanju <anutanwer12@gmail.com>
2022-07-28 14:00:56 +0530

    Time: 11 ms (70.38%), Space: 7.4 MB (7.79%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         if(size(s)!=size(t)) return false;
+       unordered_map<char,int>m;
+           for(auto &i:s)m[i]++;
+           for(auto &i:t)m[i]--;
+        for(auto &i:m)
+            if(i.second!=0)
+                return false;
+        return true;
+    }
+};
